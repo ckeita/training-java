92c1586e9b92e96be44f49e6378eb68647fd7814
Chegou Jr KEITA <ckeita@e-biz.fr>
2017-05-09 17:48:05 +0200

    [SCRIPT] add volume

--- cdb-job.sh
+++ cdb-job.sh
@@ -6,3 +6,4 @@ SRC_DIR="/home/ebiz/Projects/training-java/computer-database"
 
 # Build package
 docker run -i --rm --name cdb-project -v "$SRC_DIR":/home/cdb -w /home/cdb maven:3.5.0-jdk-8 mvn clean package
+
