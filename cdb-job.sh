dfdcd0f8059ba7af5ac0030111cf7cd4b60a54b4
Chegou Jr. Keita <ckeita@e-biz.fr>
2017-05-10 00:05:33 +0200

    [FIX] script crash

--- cdb-job.sh
+++ cdb-job.sh
@@ -5,7 +5,7 @@ SRC_DIR="/media/Data/My_Projects/training-java/computer-database"
 docker run -i --rm --name cdb-project -v "$SRC_DIR":/home/cdb -w /home/cdb maven:3.5.0-jdk-8 mvn clean package
 
 # Build tomcat image
-docker built -t tomcat-cdb .
+docker build -t tomcat-cdb .
 
 # run tomcat cdb
 docker run -i --rm --name tomcat-cdb -p 8081:8080 tomcat:8.0.43-jre8
