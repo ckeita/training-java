7365349a0fd8cdda43ce721a666cfb6600571bf2
Chegou Jr KEITA <ckeita@e-biz.fr>
2017-05-09 17:07:29 +0200

    [SCRIPT] update script

--- cdb-job.sh
+++ cdb-job.sh
@@ -5,4 +5,4 @@ SRC_DIR="/home/ebiz/Projects/training-java/computer-database"
 
 
 # Build package
-docker run -it --rm --name cdb-project -v "$SRC_DIR":/home/cdb -w /home/cdb maven:3.5.0-jdk-8 mvn clean package
+docker run -i --rm --name cdb-project -v "$SRC_DIR":/home/cdb -w /home/cdb maven:3.5.0-jdk-8 mvn clean package
