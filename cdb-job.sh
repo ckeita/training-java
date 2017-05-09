0a2d3fe9ba75449e0b82d6bcda00f2753ffefce2
Chegou Jr. Keita <ckeita@e-biz.fr>
2017-05-09 23:39:32 +0200

    [SCRIPT] add job

--- cdb-job.sh
+++ cdb-job.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
-
 # src directory
 SRC_DIR="/home/ebiz/Projects/training-java/computer-database"
 
-
-# Build package
+# Build cdb package
 docker run -i --rm --name cdb-project -v "$SRC_DIR":/home/cdb -w /home/cdb maven:3.5.0-jdk-8 mvn clean package
+
+# Build tomcat image
+docker built -t tomcat-cdb .
+
+# run tomcat cdb
+docker run -i --rm --name tomcat-cdb -p 8081:8080 tomcat:8.0.43-jre8
 
