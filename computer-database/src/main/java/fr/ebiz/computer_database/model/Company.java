9536910e8f8f1c70a78d48f178025703390811e1
Chegou Jr. Keita <keita@informatique.univ-paris-diderot.fr>
2017-05-26 22:51:29 +0200

    [IMPLEM] start hibernate implementation

--- computer-database/src/main/java/fr/ebiz/computer_database/model/Company.java
+++ computer-database/src/main/java/fr/ebiz/computer_database/model/Company.java
@@ -3,11 +3,13 @@ package fr.ebiz.computer_database.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author ckeita
  */
 @Entity
+@Table(name="company")
 public class Company {
 
     @Id
